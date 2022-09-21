be2c91161f83ebd2054ca9e793ff5bbe1f7342fd
Brad Teague <b_rad88@live.com>
2022-09-21 23:03:45 +0930

    Provide blog data to homepage template

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const { Blog, User, Comment } = require("../model");
 
-router.get("/", (req, res) => {
-  res.render("homepage");
+router.get("/", async (req, res) => {
+  const blogData = await Blog.findAll({
+    include: [{ model: User, as: "author" }, { model: Comment }],
+  });
+  const blogs = blogData.map(model => model.get({ plain: true }));
+  res.render("homepage", { blogs });
+});
+
+router.get("/signup", async (req, res) => {
+  res.render("signup");
+});
+
+router.get("/dashboard", async (req, res) => {
+  res.render("dashboard");
 });
 
 module.exports = router;
