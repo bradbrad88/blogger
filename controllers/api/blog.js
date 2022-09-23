fb21f0dfc1e37a00c2b6576ff633a71dfe601406
Brad Teague <b_rad88@live.com>
2022-09-23 21:24:03 +0930

    Create blog api routes

--- controllers/api/blog.js
+++ controllers/api/blog.js
@@ -1,10 +1,13 @@
 const { Blog, Comment } = require("../../model");
-
+const HandledError = require("../../error/Error");
 const router = require("express").Router();
 
 router.post("/", async (req, res, next) => {
   try {
-    const blog = await Blog.create(req.body);
+    const { text, title } = req.body;
+    const { user_id } = req.session;
+    if (!text || !title) return next(HandledError.badRequest());
+    const blog = await Blog.create({ title, text, user_id });
     res.json(blog);
   } catch (error) {
     next(error);
@@ -16,8 +19,6 @@ router.post("/:blog_id/comment", async (req, res, next) => {
     const { user_id } = req.session;
     const { blog_id } = req.params;
     const { content } = req.body;
-    console.log(req.body);
-    console.log(user_id, blog_id, content);
     const comment = await Comment.create({
       content,
       user_id,
@@ -27,6 +28,28 @@ router.post("/:blog_id/comment", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+router.put("/:blog_id", async (req, res, next) => {
+  try {
+    const { blog_id } = req.params;
+    const blog = await Blog.findByPk(blog_id);
+    if (!blog) return next(HandledError.notFound());
+    if (blog.user_id !== req.session.user_id) return next(HandledError.unauthorised());
+    await blog.update(req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:blog_id", async (req, res, next) => {
+  const { blog_id } = req.params;
+  const blog = await Blog.findByPk(blog_id);
+  if (!blog) return next(HandledError.notFound());
+  if (blog.user_id !== req.session.user_id) return next(HandledError.unauthorised());
+  await blog.destroy();
+  res.sendStatus(200);
 });
 
 module.exports = router;
