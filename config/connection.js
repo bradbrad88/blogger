852cfc060088761a189df01aa779a638edd1893f
Brad Teague <b_rad88@live.com>
2022-09-24 11:50:03 +0930

    Update sequelize connection to handle url connection string

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,17 @@ require("dotenv").config();
 
 let sequelize;
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
-sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: "mysql",
-  port: 3306,
-});
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
+console.log(DATABASE_URL);
+if (DATABASE_URL) {
+  console.log("Connecting to DB with URL string");
+  sequelize = new Sequelize(DATABASE_URL);
+} else {
+  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: "mysql",
+    port: 3306,
+  });
+}
 
 module.exports = sequelize;
